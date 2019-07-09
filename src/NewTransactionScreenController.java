df24a2c17511a91211da7e8c442924834e6c5abb
Prabjot Singh <singhp2@unlv.neavda.edu>
2019-07-09 11:27:11 -0700

    Created NewTransactionScreen and NewTransactionScreenController

---
+++ src/NewTransactionScreenController.java
@@ -0,0 +1,55 @@
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleGroup;
+
+public class NewTransactionScreenController {
+
+    @FXML
+    private Button buttonLogoutCheckingAccount;
+
+    @FXML
+    private TextField textboxTransferToAccountNumber;
+
+    @FXML
+    private Button buttonCreditCardCheckingAccount;
+
+    @FXML
+    private Label labelAccountBalance;
+
+    @FXML
+    private RadioButton radiobuttonWithdraw;
+
+    @FXML
+    private RadioButton radiobuttonTransfer;
+
+    @FXML
+    private Button buttonCheckingAccountCheckingAccount;
+
+    @FXML
+    private Button buttonReportsCheckingAccount;
+
+    @FXML
+    private Button buttonCancelTransaction;
+
+    @FXML
+    private RadioButton radiobuttonDeposit;
+
+    @FXML
+    private ToggleGroup groupTransactionType;
+
+    @FXML
+    private Button buttonSaveTransaction;
+
+    @FXML
+    private Button buttonUserAccountCheckingAccount;
+
+    @FXML
+    private Label labelAccountNumber;
+
+    @FXML
+    private TextField textboxTransactionAmount;
+
+}
