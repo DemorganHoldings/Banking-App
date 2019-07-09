e65b36610010a59eff87ca58f340d47563becb7d
satindercj <satindercj@gmail.com>
2019-07-09 14:16:32 -0700

    Information is passed to New Transaction Controller

--- src/NewTransactionScreenController.java
+++ src/NewTransactionScreenController.java
@@ -1,9 +1,8 @@
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.RadioButton;
-import javafx.scene.control.TextField;
-import javafx.scene.control.ToggleGroup;
+import javafx.scene.control.*;
+
+import java.sql.*;
+import java.text.DecimalFormat;
 
 public class NewTransactionScreenController {
 
@@ -50,6 +49,53 @@ public class NewTransactionScreenController {
     private Label labelAccountNumber;
 
     @FXML
+    private Label labelCustomerName;
+
+    @FXML
     private TextField textboxTransactionAmount;
+
+    private String customerID;
+
+    // method for initializing the window
+    public void initialize(String id) {
+        customerID = id;
+
+        final String DB_URL = "jdbc:mysql://142.93.91.169:3306/spDemorganDB";
+        final String USERNAME = "root";
+        final String PASSWORD = "password123";
+
+        DecimalFormat moneyFormat = new DecimalFormat("$#,##0.00");
+
+        try {
+            Connection conn = DriverManager.getConnection(DB_URL, USERNAME, PASSWORD); //Establish database connection
+            Statement stmt = conn.createStatement(); //Create new statement object
+
+            String sql = "SELECT Name from Customer WHERE CustomerID='" + customerID + "'";
+
+            ResultSet rs = stmt.executeQuery(sql);
+
+            //Display the content of the result set
+            while (rs.next()) {
+                labelCustomerName.setText(rs.getString("Name"));
+            }
+
+            sql = "SELECT * from CheckingAccount WHERE CustomerID='" + customerID + "'";
+
+            rs = stmt.executeQuery(sql);
+
+            //Display the content of the result set
+            while (rs.next()) {
+                labelAccountNumber.setText(rs.getString("AccountNumber"));
+                double bal = Double.parseDouble(rs.getString("Balance"));
+                labelAccountBalance.setText(moneyFormat.format(bal));
+            }
+
+            conn.close();
+
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
+
+    }
 
 }
