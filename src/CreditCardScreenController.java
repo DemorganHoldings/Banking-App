da2b87ea2a15bf98cefcfc826b6d3fd55303b3f5
Prabjot Singh <singhp2@unlv.neavda.edu>
2019-07-10 00:53:58 -0700

    Created CreditCardScreen and ReportScreen with respective controllers. Also included background image for Credit Card Design.

---
+++ src/CreditCardScreenController.java
@@ -0,0 +1,52 @@
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.event.ActionEvent;
+
+public class CreditCardScreenController {
+
+    @FXML
+    private Label labelCreditCardExpiration;
+
+    @FXML
+    private Button buttonLogoutCheckingAccount;
+
+    @FXML
+    private Button buttonCreditCardCheckingAccount;
+
+    @FXML
+    private Label labelAccountBalance;
+
+    @FXML
+    private Button buttonCheckingAccountCheckingAccount;
+
+    @FXML
+    private Button buttonReportsCheckingAccount;
+
+    @FXML
+    private Button buttonApplyForCreditCard;
+
+    @FXML
+    private Label labelCreditCardNumber;
+
+    @FXML
+    private Button buttonUserAccountCheckingAccount;
+
+    @FXML
+    private TextField textboxCreditScore;
+
+    @FXML
+    private Label labelAccountNumber;
+
+    @FXML
+    private Label labelCreditCardCvvCode;
+
+    @FXML
+    private Label labelCustomerName;
+
+    @FXML
+    void e91e63(ActionEvent event) {
+
+    }
+}
